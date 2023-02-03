d2c407c3d2fb321fe08c3c91cd5f70e667dff9fa
andreyfranata <86184444+andreyfranata@users.noreply.github.com>
2023-02-03 16:07:37 +0700

    e

--- language/english.js
+++ language/english.js
@@ -179,7 +179,7 @@ exports.AdMsg = (text) => {
 exports.DoneMsg = (prefix, text) => {
     return `Successfully added message in message list as '${text}'
     
-Access with ${prefix}${text}
+Access with ${prefix}get ${text}
 View list of Messages With ${prefix}listmsg`
 }
 exports.GetMsg = (prefix, command) => {
